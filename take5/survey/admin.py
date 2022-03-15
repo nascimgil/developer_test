dea579b3df7a4d7ab318a3bb9494a716a8212dab
nascimgil <nascimgil@gmail.com>
2022-03-15 13:34:55 -0300

    funcionalidade de busca via admin

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Survey)
-admin.site.register(SurveyQuestion)
-admin.site.register(SurveyQuestionAlternative)
-admin.site.register(SurveyUserAnswer)
+class SurveyAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'description']
+
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    search_fields = ['questionID', 'surveyID', 'question_title']
+
+class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
+    search_fields = [ 'surveyID','questionID', 'choice_text']
+
+class SurveyUserAnswerAdmin(admin.ModelAdmin):
+    search_fields = [ 'surveyID','questionID', 'user_answerID', 'user','body','choice_text']
+
+
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
+admin.site.register(SurveyQuestionAlternative, SurveyQuestionAlternativeAdmin)
+admin.site.register(SurveyUserAnswer, SurveyUserAnswerAdmin)
+
